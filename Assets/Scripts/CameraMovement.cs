fa9cf850977cde97f4c3e06eb1281936180b28bc
hfonseca2177 <hfonseca21@gmail.com>
2021-01-20 13:06:43 -0300

    Adding traditional 2d char to test light and camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 newCameraPosition = new Vector3(target.position.x,target.position.y, transform.position.z);
+            newCameraPosition.x = Mathf.Clamp(newCameraPosition.x, minPosition.x, maxPosition.x);
+            newCameraPosition.y = Mathf.Clamp(newCameraPosition.y, minPosition.y, maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position, newCameraPosition, smoothing);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D _rigidBody;
+    private Vector3 _change;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _change = Vector3.zero;
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        _change.x = Input.GetAxisRaw("Horizontal");
+        _change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimation();
+
+    }
+
+    void MoveCharacter()
+    {        
+        _rigidBody.MovePosition(transform.position + (_change * speed * Time.deltaTime));
+    }
+
+    void UpdateAnimation()
+    {
+        if (_change != Vector3.zero)
+        {
+            MoveCharacter();
+            _animator.SetFloat("moveX", _change.x);
+            _animator.SetFloat("moveY", _change.y);
+            _animator.SetBool("moving", true);
+        }
+        else
+        {
+            _animator.SetBool("moving", false);
+        }
+    }
+}
