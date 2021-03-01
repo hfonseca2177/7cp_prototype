3a423b0cc46d4e89da6f1427fc1406088870d964
hfonseca2177 <hfonseca21@gmail.com>
2021-03-01 00:38:16 -0300

    scene side walk desert thidia

---
+++ Assets/Scripts/PlayerSideMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSideMovement : MonoBehaviour
+{
+    public float speed;    
+    float moveVelocity;
+
+    void Update()
+    {
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+}
